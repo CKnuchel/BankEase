4da902bb3ca35e2ef8cd6087fb2941121a2ee335
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-10-28 13:46:49 +0100

    ADDED: Home Controller

--- BankEase/Controllers/HomeController.cs
+++ BankEase/Controllers/HomeController.cs
@@ -1,32 +1,49 @@
-using System.Diagnostics;
+using BankEase.Data;
+using BankEase.Messages.HomeMessages;
 using BankEase.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankEase.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController(DatabaseContext context) : Controller
+    {
+        #region Fields
+        private readonly DatabaseContext _context = context;
+        #endregion
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        #region Publics
+        public async Task<IActionResult> Index()
+        {
+            // Laden der Customers
+            List<Customer> customers = await _context.Customers.ToListAsync();
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+            List<SelectListItem> customerOptions = customers.Select(customer => new SelectListItem
+                                                                                {
+                                                                                    Value = customer.Id.ToString(),
+                                                                                    Text = $"{customer.FirstName} {customer.LastName}"
+                                                                                }).ToList();
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+            this.ViewBag.CustomerOptions = customerOptions;
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(int? user)
+        {
+            if(user is null or 0)
+            {
+                this.ModelState.AddModelError("user", HomeMessages.LoginUserNotSelected);
+                return RedirectToAction("Index");
+            }
+
+            // Benutzer in der Session speichern
+            this.HttpContext.Session.SetInt32("user", user.Value);
+
+            return RedirectToAction("Index", "Account");
+        }
+        #endregion
+    }
 }
---
+++ BankEase/Messages/HomeMessages/HomeMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace BankEase.Messages.HomeMessages
+{
+    public static class HomeMessages
+    {
+        #region Properties
+        public static string LoginUserNotSelected => "Bitte wählen Sie einen gültigen Benutzer aus.";
+        #endregion
+    }
+}
