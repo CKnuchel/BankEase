505826a8e5ae26c1141071410ea203383c6eebc6
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-10-28 10:42:02 +0100

    ADDED: Account model

--- BankEase/Data/DatabaseContext.cs
+++ BankEase/Data/DatabaseContext.cs
@@ -6,7 +6,8 @@ namespace BankEase.Data
 	public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
 	{
 		#region Properties
-		private DbSet<Customer> Customer { get; set; }
+		public DbSet<Customer> Customer { get; set; }
+		public DbSet<Account> Account { get; set; }
 		#endregion
 
 		#region Protecteds
@@ -14,9 +15,15 @@ namespace BankEase.Data
 		{
 			base.OnModelCreating(modelBuilder);
 
+			// Die CustomerNumber auf Unique stelen
 			modelBuilder.Entity<Customer>()
 			            .HasIndex(e => e.CustomerNumber)
 			            .IsUnique();
+
+			// Die IBAN auf Unique stellen
+			modelBuilder.Entity<Account>()
+						.HasIndex(e => e.IBAN)
+						.IsUnique();
 		}
 		#endregion
 	}
---
+++ BankEase/Models/Account.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankEase.Models
+{
+	[Table("ACCOUNT")]
+	public class Account
+	{
+		#region Properties
+		[Key]
+		[Column(name: "ID")]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(30)]
+		[Column(name: "IBAN")]
+		public required string IBAN { get; set; }
+
+		[Required]
+		[Column(name: "BALANCE", TypeName = "decimal(9,2)")]
+		public decimal Balance { get; set; }
+
+		[Required]
+		[Column(name: "OVERDRAFT", TypeName = "decimal(7,2)")]
+		public decimal Overdraft { get; set; }
+
+		[Required]
+		[Column(name: "CUSTOMER_ID")]
+		public int CustomerId { get; set; }
+
+		[ForeignKey("CustomerId")]
+		public required Customer Customer { get; set; }
+		#endregion
+	}
+}
