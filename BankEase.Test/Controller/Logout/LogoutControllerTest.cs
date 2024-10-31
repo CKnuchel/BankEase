a0fe6e15a2bc03a023fa3d0a2527b31ce3dde833
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-10-31 11:20:05 +0100

    ADDED: Logout Button

---
+++ BankEase.Test/Controller/Logout/LogoutControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using BankEase.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace BankEase.Test.Controller.Logout;
+
+[TestClass]
+public class LogoutControllerTest
+{
+    #region Fields
+    private LogoutController _controller = null!;
+    private Mock<ISession> _sessionMock = null!;
+    private Mock<HttpContext> _httpContextMock = null!;
+    #endregion
+
+    #region Initialize and Cleanup
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        // Mock für die Sitzung
+        _sessionMock = new Mock<ISession>();
+        _httpContextMock = new Mock<HttpContext>();
+        _httpContextMock.Setup(context => context.Session).Returns(_sessionMock.Object);
+
+        // Controller instanziieren und HttpContext zuweisen
+        _controller = new LogoutController
+                      {
+                          ControllerContext = new ControllerContext
+                                              {
+                                                  HttpContext = _httpContextMock.Object
+                                              }
+                      };
+    }
+    #endregion
+
+    #region Tests
+    [TestMethod]
+    public void Logout_ClearsSessionAndRedirectsToHome()
+    {
+        // Act
+        IActionResult result = _controller.Logout();
+
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+
+        //  Verifizieren, ob die Sitzung gelöscht wurde
+        _sessionMock.Verify(session => session.Clear(), Times.Once);
+
+        // Überprüfen, ob die Weiterleitung zur Startseite erfolgt
+        RedirectToActionResult redirectResult = (RedirectToActionResult) result;
+        Assert.AreEqual("Index", redirectResult.ActionName);
+        Assert.AreEqual("Home", redirectResult.ControllerName);
+    }
+    #endregion
+}
---
+++ BankEase/Controllers/LogoutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankEase.Controllers
+{
+    public class LogoutController : Controller
+    {
+        #region Publics
+        public IActionResult Logout()
+        {
+            this.HttpContext.Session.Clear();
+
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+    }
+}
