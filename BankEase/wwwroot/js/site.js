644eda844e1be5fa168aaf35d7675c1c87189e90
Christoph Knuchel <christoph.knuchel@m-s.ch>
2024-11-05 10:21:35 +0100

    CHANGED: Moved JavaScript in script.js File, to reduce duplicated Code

--- BankEase/wwwroot/js/site.js
+++ BankEase/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿// Verhindert das einfügen des Buchstaben 'E', sowie der -/+ Zeichen
+function preventInvalidNumberInputs(event)
+{
+    if(["e", "E", "+", "-"].includes(event.key))
+    {
+        event.preventDefault();
+    }
+}
